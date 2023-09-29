a4abbbb990046549d3e72aa6881796552409e4f3
Mohamed ABID <mabid@made-f0Ar8s7.clusters.42paris.fr>
2023-09-29 18:45:47 +0200

    the api run but the db doesn't recognize the database url so we have to fix it

--- lourd/src/main.ts
+++ lourd/src/main.ts
@@ -2,9 +2,27 @@
 
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { config } from 'dotenv';
+import { PrismaClient } from '@prisma/client';
+
+
+//config();
+require('dotenv').config(); // Chargez les variables d'environnement depuis le fichier .env
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+
+  const prisma = new PrismaClient();
+/*   const result = await prisma.user.findMany({
+    include: {
+      posts: true,
+    },
+  }); */
 }
 bootstrap();
+/* // Dans votre code
+const dotenv = require('dotenv');
+dotenv.config(); // Charge les variables d'environnement depuis le fichier .env
+console.log(process.env.DATABASE_URL); // Vérifiez si DATABASE_URL est définie */
--- lourd/src/prisma/prisma.service.ts
+++ lourd/src/prisma/prisma.service.ts
@@ -2,8 +2,14 @@ import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
-  async onModuleInit() {
-    await this.$connect();
+export class PrismaService extends PrismaClient {
+  constructor() {
+    super({
+      datasources: {
+        db: {
+          url: process.env.DATABASE_URL,
+        },
+      },
+    });
   }
 }
