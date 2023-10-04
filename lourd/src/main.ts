f186df5d47a3b1e96a71150d79df1be29ddbc3b4
Mohamed ABID <mabid@paul-f3Ar5s3.clusters.42paris.fr>
2023-10-04 17:47:20 +0200

    same problem as before, we add some stuff and fix other. Specially docker part, 2

--- lourd/src/main.ts
+++ lourd/src/main.ts
@@ -6,23 +6,14 @@ import { config } from 'dotenv';
 import { PrismaClient } from '@prisma/client';
 
 
-//config();
-require('dotenv').config(); // Chargez les variables d'environnement depuis le fichier .env
-
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 
   const prisma = new PrismaClient();
-/*   const result = await prisma.user.findMany({
-    include: {
-      posts: true,
-    },
-  }); */
 }
-bootstrap();
 // Dans votre code
 const dotenv = require('dotenv');
 dotenv.config(); // Charge les variables d'environnement depuis le fichier .env
 console.log(process.env.DATABASE_URL); // Vérifiez si DATABASE_URL est définie
+bootstrap();
--- lourd/src/user/user.service.ts
+++ lourd/src/user/user.service.ts
@@ -1,56 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
-import { User, Prisma } from '@prisma/client';
+import { User} from '@prisma/client';
+import { HttpException, HttpStatus } from '@nestjs/common';
+
 
 @Injectable()
 export class UserService {
   constructor(private prisma: PrismaService) {}
 
-  async user(
-    userWhereUniqueInput: Prisma.UserWhereUniqueInput,
-  ): Promise<User | null> {
-    return this.prisma.user.findUnique({
-      where: userWhereUniqueInput,
-    });
-  }
-
-  async users(params: {
-    skip?: number;
-    take?: number;
-    cursor?: Prisma.UserWhereUniqueInput;
-    where?: Prisma.UserWhereInput;
-    orderBy?: Prisma.UserOrderByWithRelationInput;
-  }): Promise<User[]> {
-    const { skip, take, cursor, where, orderBy } = params;
-    return this.prisma.user.findMany({
-      skip,
-      take,
-      cursor,
-      where,
-      orderBy,
-    });
-  }
-
-  async createUser(data: Prisma.UserCreateInput): Promise<User> {
-    return this.prisma.user.create({
-      data,
-    });
-  }
-
-  async updateUser(params: {
-    where: Prisma.UserWhereUniqueInput;
-    data: Prisma.UserUpdateInput;
-  }): Promise<User> {
-    const { where, data } = params;
-    return this.prisma.user.update({
-      data,
-      where,
-    });
-  }
-
-  async deleteUser(where: Prisma.UserWhereUniqueInput): Promise<User> {
-    return this.prisma.user.delete({
-      where,
-    });
+  async createUser(userData: any): Promise<User> {
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          id: userData.id,
+          name: userData.name,
+          email: userData.email,
+          password: userData.password,
+        }
+      });
+      return user;
+    } catch (error) {
+      console.error(error);
+      throw new HttpException('Failed to create user', HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 }
